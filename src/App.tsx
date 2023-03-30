1328605ae2ce05ac023aaf53f885b85414f4dd1d
ayush-shta <ayushshta@gmail.com>
2023-03-30 15:24:53 +0545

    install axios and react query and setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import AppRouter from "./app-router";
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <AppRouter />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRouter />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ src/services/http.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+import { API_BASE_URL } from "src/constants/config";
+
+export const httpInstance = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 0,
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
+});
